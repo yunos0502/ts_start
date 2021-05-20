68a77c21df7af2075caccb6e0dc6093e85727931
Park Gyu Ae <yunos0502@gmail.com>
2021-05-20 21:15:40 +0900

    Mapped

---
+++ src/5_mapped/7.ts
@@ -0,0 +1,13 @@
+export {};
+
+enum Fruit {
+  Apple,
+  Banana,
+  Orange,
+}
+
+const FRUIT_PRICE = {
+  [Fruit.Apple]: 1000,
+  [Fruit.Banana]: 1500,
+  [Fruit.Orange]: 2000,
+}
---
+++ src/5_mapped/8.ts
@@ -0,0 +1,14 @@
+export {};
+
+enum Fruit {
+  Apple,
+  Banana,
+  Orange,
+}
+
+const FRUIT_PRICE: { [key in Fruit]: number } = {
+  [Fruit.Apple]: 1000,
+  [Fruit.Banana]: 1500,
+  [Fruit.Orange]: 2000,
+  // [Fruit.Orange2]: 2000,
+}
