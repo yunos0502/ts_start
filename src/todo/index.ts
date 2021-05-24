89cb9870687b7332a786d6ba97c8257a502f7a04
Park Gyu Ae <yunos0502@gmail.com>
2021-05-24 23:34:35 +0900

    feat: Add todo project

---
+++ src/todo/index.ts
@@ -0,0 +1,57 @@
+import chalk from "chalk";
+import { Command, CommandDeleteTodo, CommandNewTodo, CommandPrintTodos } from "./Command";
+import { waitForInput } from "./Input";
+import Todo from "./Todo";
+import { Action, AppState, Priority } from "./type";
+
+const commands: Command[] = [new CommandPrintTodos(), new CommandNewTodo(), new CommandDeleteTodo()];
+
+async function main() {
+  let state: AppState = {
+    todos: [
+      new Todo('test1', Priority.High),
+      new Todo('test2', Priority.Medium),
+      new Todo('test3', Priority.Low),
+    ]
+  }
+
+  while(true) {
+    console.clear();
+
+    for (const command of commands) {
+      console.log(command.toString());
+    }
+
+    console.log();
+
+    const key = await waitForInput('input command: ');
+
+    console.clear();
+
+    const command = commands.find(item => item.key === key);
+    
+    if (command) {
+      const action = await command.run(state);
+      if (action) {
+        state = getNextState(state, action);
+      }
+    }
+  }
+}
+
+main();
+
+function getNextState(state: AppState, action: Action): AppState {
+  switch(action.type) {
+    case 'newTodo':
+      return {
+        ...state,
+        todos: [...state.todos, new Todo(action.title, action.priority)]
+      }
+    case 'deleteTodo':
+      return {
+        ...state,
+        todos: state.todos.filter(item => item.id !== action.id)
+      }
+  }
+}
